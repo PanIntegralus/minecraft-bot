345497690df864622a2ff9a3e73ae1a1cbf4176b
PanIntegralus <42538693+PanIntegralus@users.noreply.github.com>
2022-02-07 23:04:03 +0100

    añadir un par de cosita ma

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cleverbot = require("cleverbot-free");
 
 const mineflayer = require('mineflayer')
+const { pathfinder, Movements, goals: { GoalNear } } = require('mineflayer-pathfinder')
 
 require('dotenv').config()
 
@@ -13,6 +14,9 @@ const bot = mineflayer.createBot({
     version: '1.18.1',
     auth: 'microsoft'
 })
+
+bot.loadPlugin(pathfinder)
+const RANGE_GOAL = 1
 
 // const welcome = () => {
 //     bot.chat('hola buenos dia')
@@ -35,12 +39,28 @@ bot.on('chat', (username, message) => {
             getResponse(message)
         }
         if (message == prefix+'help') {
-            bot.chat('Comandos: mine, come, go')
+            bot.chat('Comandos: mine, come, stop, go')
         }
         if (message == prefix+'come') {
-
+            const target = bot.players[username]?.entity
+            if (!target) {
+                bot.chat("Estás demasiado lejos, no te veo.")
+            }
+            else {
+                const { x: playerX, y: playerY, z: playerZ } = target.position
+                bot.chat("Estoy yendo pa "+target.position)
+                bot.pathfinder.setGoal(new GoalNear(playerX, playerY, playerZ, RANGE_GOAL))
+            }
+        }
+        if (message == prefix+'stop') {
+            bot.pathfinder.stop
+            bot.chat("Me he parao")
         }
     }
+})
+
+bot.on('goal_reached', function() {
+    bot.chat("Ya he llegao")
 })
 
 bot.once('death', function() {})
